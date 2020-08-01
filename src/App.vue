0a96ed1a795dea1b4316519867887468533833fc
YuSong-ys <873608487@qq.com>
2020-08-01 12:20:40 +0800

    仿京东品优购项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <shortcut/>
+  </div>
+</template>
+
+<script>
+import shortcut from './components/shortcut'
+export default {
+  components:{
+    shortcut
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/shortcut.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="short_left">123</div>
+    <div class="short_right">abc</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'shortcut'
+}
+</script>
+
+<style>
+
+</style>
